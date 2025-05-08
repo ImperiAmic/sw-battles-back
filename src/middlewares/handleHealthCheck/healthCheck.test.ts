408c30fdc0860fd365a5fd426b03ac5381f329ea
Raimon <raimon.txollo@gmail.com>
2025-05-08 17:47:34 +0200

    Add healthCheck middleware tests

---
+++ src/middlewares/handleHealthCheck/healthCheck.test.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+import handleHealthCheck from "./healthCheck.js";
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+describe("Given the handleHealthCheck middleware", () => {
+  describe("When it receives a response", () => {
+    const req = {};
+    const res: Pick<Response, "status" | "json"> = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    test("Then it should call the received response's method status with 200", () => {
+      const expectedStatusCode = 200;
+
+      handleHealthCheck(req as Request, res as Response);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
+    });
+
+    test("Then it should call the received response's method json with '🏓' message", () => {
+      const expectedMessage = { message: "🏓" };
+
+      handleHealthCheck(req as Request, res as Response);
+
+      expect(res.json).toHaveBeenCalledWith(expectedMessage);
+    });
+  });
+});
