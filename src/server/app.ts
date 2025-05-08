9feebd6b8ff6d43bb853c9e2b2d30887f2974799
Raimon <raimon.txollo@gmail.com>
2025-05-08 22:59:54 +0200

    Add handleEndpointNotFound

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,13 +2,18 @@ import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
-import handleHealthCheck from "../middlewares/handleHealthCheck/healthCheck.js";
+import handleHealthCheck from "./middlewares/handleHealthCheck/healthCheck.js";
+import handleEndpointNotFound from "./middlewares/handleEndpointNotFound/handleEndpointNotFound.js";
+import handleErrors from "./middlewares/handleErrors/handleErrors.js";
 
 const app = express();
 
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.use(handleHealthCheck);
+app.get("/", handleHealthCheck);
+
+app.use(handleEndpointNotFound);
+app.use(handleErrors);
 
 export default app;
---
+++ src/server/middlewares/handleEndpointNotFound/handleEndpointNotFound.test.ts
@@ -0,0 +1,45 @@
+import { NextFunction, Request, Response } from "express";
+import handleEndpointNotFound from "./handleEndpointNotFound.js";
+import statusCodes from "../../../globals/statusCodes.js";
+import ServerError from "../../ServerError/ServerError.js";
+
+describe("Given the handleEndpointNotFound middleware", () => {
+  describe("When it receives a next function", () => {
+    const req = {};
+    const res = {};
+    const next = jest.fn();
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    test("Then it should call the next function with an error with status code 404", () => {
+      const expectedError = {
+        statusCode: statusCodes.NOT_FOUND,
+      } as Pick<ServerError, "statusCode">;
+
+      handleEndpointNotFound(
+        req as Request,
+        res as Response,
+        next as NextFunction,
+      );
+
+      expect(next).toHaveBeenCalledWith(expect.objectContaining(expectedError));
+    });
+
+    test("Then it should call the next function with an error with message'Endpoint not found'", () => {
+      const expectedError = { message: "Endpoint not found" } as Pick<
+        ServerError,
+        "message"
+      >;
+
+      handleEndpointNotFound(
+        req as Request,
+        res as Response,
+        next as NextFunction,
+      );
+
+      expect(next).toHaveBeenCalledWith(expect.objectContaining(expectedError));
+    });
+  });
+});
