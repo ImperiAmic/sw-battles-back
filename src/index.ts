482e03d76ef28c97f1b8acf25d6464f09cdce85f
Raimon <raimon.txollo@gmail.com>
2025-05-09 00:26:59 +0200

    Add connectionToDatabase function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import "dotenv/config";
 import startServer from "./server/startServer.js";
+import connectToDatabase from "./database/connectToDatabase.js";
 
 const port = process.env.PORT ?? 6001;
+const databaseConnectionString = process.env.DATABASE_CONNECTION_STRING;
 
+await connectToDatabase(databaseConnectionString);
 startServer(port);
