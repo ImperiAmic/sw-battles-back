8fe0904f50a21bed77856be39d44731a119140ce
Raimon <raimon.txollo@gmail.com>
2025-05-08 17:46:06 +0200

    Add healthCheck middleware

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,9 +1,12 @@
 import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
+import handleHealthCheck from "../middlewares/handleHealthCheck/healthCheck.js";
 
 const app = express();
 
 app.use(morgan("dev"));
+
+app.use(handleHealthCheck);
 
 export default app;
