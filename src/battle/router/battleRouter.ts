351a6f9e9d6d7e5349465465f8ffe696113c502b
Raimon <raimon.txollo@gmail.com>
2025-05-14 16:17:10 +0200

    Add PATCH /battles/:battleId endpoint

--- src/battle/router/battleRouter.ts
+++ src/battle/router/battleRouter.ts
@@ -7,5 +7,6 @@ const battlesRouter = Router();
 const battleController = new BattleController(Battle);
 
 battlesRouter.get("/", battleController.getBattles);
+battlesRouter.patch("/:battleId", battleController.updateBattleWinner);
 
 export default battlesRouter;
